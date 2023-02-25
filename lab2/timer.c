f2d48af8b58a5f3bd4ccc0729728724b7ae6c002
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2023-02-25 20:32:29 +0000

    Implemented the first part of lab2

---
+++ lab2/timer.c
@@ -0,0 +1,113 @@
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
+
+#include <stdint.h>
+
+#include "i8254.h"
+
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+  uint8_t st;
+  if(timer_get_conf(timer, &st)){
+      printf("Error getting config from timer %d\n", timer);
+      return 1;
+  }
+
+  uint8_t control_word;
+
+  switch (timer){
+      case 0:
+          control_word = (TIMER_SEL0 | TIMER_LSB_MSB | (st & 0x0F)); 
+          break;
+      case 1:
+          control_word = (TIMER_SEL1 | TIMER_LSB_MSB | (st & 0x0F)); 
+          break;
+      case 2:
+          control_word = (TIMER_SEL2 | TIMER_LSB_MSB | (st & 0x0F)); 
+          break;
+  }
+
+  if(sys_outb(TIMER_CTRL, control_word)){
+      printf("Error writing to timer control register\n");
+      return 1;
+  }
+
+  uint16_t counter = TIMER_FREQ / freq;
+  uint8_t lsb, msb;
+  util_get_LSB(counter, &lsb);
+  util_get_MSB(counter, &msb);
+
+  if(sys_outb(TIMER_0 + timer, lsb)) {printf("Error writing lsb to timer %d\n", timer); return 1;}
+  if(sys_outb(TIMER_0 + timer, msb)) {printf("Error writing msb to timer %d\n", timer); return 1;}
+
+  return 0;
+}
+
+int (timer_subscribe_int)(uint8_t *bit_no) {
+    /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (timer_unsubscribe_int)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+void (timer_int_handler)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+}
+
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+    if(sys_outb(TIMER_CTRL, TIMER_RB_SEL(timer) | TIMER_RB_COUNT_ | TIMER_RB_CMD)){
+        printf("Error in writing to timer control register\n");
+        return 1;
+    }
+
+    if(util_sys_inb(TIMER_0 + timer, st)){
+        printf("Error reading config from timer %d\n", timer);}
+
+    return 0;
+}
+
+int (timer_display_conf)(uint8_t timer, uint8_t st,
+                        enum timer_status_field field) {
+  
+  union timer_status_field_val val;
+  uint8_t mode;
+
+  switch (field){
+      case tsf_all:
+          val.byte = st;
+          break;
+
+      case tsf_initial:
+          mode = (st & (BIT(4) | BIT(5))) >> 4;
+
+          if(mode==BIT(0)){val.in_mode=LSB_only;}
+          else if(mode==BIT(1)){val.in_mode=MSB_only;}
+          else if(mode== (BIT(0) | BIT(1))){val.in_mode=MSB_after_LSB;}
+          else{val.in_mode = INVAL_val;}
+          break;
+
+      case tsf_mode:
+          mode = (st & (BIT(1) | BIT(2) | BIT(3))) >> 1;
+          if(mode==6){val.count_mode=2;}
+          else if(mode==7){val.count_mode=3;}
+          else{val.count_mode=mode;}
+          break;
+
+      case tsf_base:
+          val.bcd = st & 1;
+          break;
+
+    default:
+      printf("Invalid field\n");
+      return 1;
+  }
+
+  return timer_print_config(timer, field, val);
+}
