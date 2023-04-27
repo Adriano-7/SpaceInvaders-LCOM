4169ec031198e5bd01a13f6fd8f7e029255f6565
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2023-04-27 14:03:31 +0100

    Finished function video_test_rectangle()

--- lab5/graphics.c
+++ lab5/graphics.c
@@ -1,62 +1,9 @@
 #include "graphics.h"
 
-<<<<<<< Updated upstream
-static void *video_mem;
-vbe_mode_info_t info_mode;
-struct minix_mem_range memory_range;
+struct minix_mem_range mem_range;
+vbe_mode_info_t mode_info;
+uint8_t* video_mem;
 
-unsigned int vram_base;
-unsigned int vram_size;
-int r;
-
-int set_video_mem(uint16_t mode){
-  memset(&info_mode, 0, sizeof(info_mode));
-
-  if(vbe_get_mode_info(mode, &info_mode) != 0){
-    printf("vbe_get_mode_info() -> error getting video mode\n");
-    return 1;
-  }
-
-  int hres = info_mode.XResolution;
-  int vres = info_mode.YResolution;
-  int bits_per_pixel = info_mode.BitsPerPixel;
-
-  vram_base = info_mode.PhysBasePtr;
-  vram_size = hres*vres*(bits_per_pixel+7)/8;
-
-  memory_range.mr_base = info_mode.PhysBasePtr;
-  memory_range.mr_limit = memory_range.mr_base + vram_size;
-
-  if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &memory_range)))
-    panic("sys_privctl (ADD_MEM) failed: %d\n", r);
-
-  video_mem = vm_map_phys(SELF, (void *)memory_range.mr_base, vram_size);
-
-  if(video_mem == MAP_FAILED){
-    panic("couldn't map video memory");
-  }
-
-  return 0;
-}
-
-int set_graphics_mode(uint16_t submode){
-  reg86_t r86;
-  memset(&r86, 0, sizeof(r86));
-
-  r86.intno = 0x10;
-  r86.ah = 0x4F;
-  r86.al = 0x02; 
-
-  r86.ax = 0x4F02;
-  r86.bx = submode | BIT(14);
-
-  if( sys_int86(&r86) != OK ) {
-    printf("\tvg_exit(): sys_int86() failed \n");
-    return 1;
-  }
-  return 0;
-}
-=======
 int (set_graphics_mode)(uint16_t submode) {
   reg86_t reg;
   memset(&reg, 0, sizeof(reg));
@@ -65,11 +12,85 @@ int (set_graphics_mode)(uint16_t submode) {
   reg.al = GR_AL;
   reg.bx = GR_BX(submode);
   reg.intno = GR_INTNO;
->>>>>>> Stashed changes
-
+  
   if(sys_int86(&reg) != OK){
     printf("Error setting up graphics mode\n");
     return 1;
+  }
+  return 0;
+}
+
+int map_phys_mem(uint16_t mode){
+  memset(&mode_info, 0, sizeof(mode_info));
+  if(vbe_get_mode_info(mode, &mode_info)){
+    printf("Error obtaining mode_info\n");
+    return 1;
+  }
+
+  int BytesPerPixel = (mode_info.BitsPerPixel+7)/8;
+  int vram_base = mode_info.PhysBasePtr;
+  int vram_size = (mode_info.XResolution*mode_info.YResolution)*BytesPerPixel;
+
+  mem_range.mr_base = vram_base;
+  mem_range.mr_limit = mode_info.PhysBasePtr + vram_size;
+
+  if(OK != sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mem_range)){
+      printf("Error mapping physical to virtual memory\n");
+      return 1;
+  }
+
+  video_mem = vm_map_phys(SELF, (void *)mem_range.mr_base, vram_size);
+  if(video_mem == MAP_FAILED){
+    printf("Couldn't map video memory\n");
+    return 1;
+  }
+  return 0;
+}
+
+int (pixel_index)(uint16_t x, uint16_t y, uint16_t BytesPerPixel){
+  if(x>mode_info.XResolution){
+    printf("The x position is greater than the resolution allowed\n");
+    return -1;
+  }
+
+  if(y>mode_info.YResolution){
+    printf("The y position is greater than the resolution allowed\n");
+    return -1;
+  }
+
+  return (y*mode_info.XResolution+x)*BytesPerPixel;
+}
+
+int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color){
+  uint16_t BytesPerPixel = (mode_info.BitsPerPixel+7)/8;
+
+  int index = pixel_index(x, y, BytesPerPixel);
+  if(index==-1){printf("Coordinate not allowed\n");}
+
+  if(memcpy(&video_mem[index], &color, BytesPerPixel)==NULL){
+    printf("Error drawing pixel\n");
+    return 1;
+  }
+
+  return 0;
+}
+
+int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color){
+  for(unsigned i=0; i<len; i++){
+    if(vg_draw_pixel(x+i, y, color)){
+      printf("Error pixel with coordinates (%d, %d)\n", x+i, y);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color){
+  for(unsigned i=0; i<height; i++){
+    if(vg_draw_hline(x, y+i, width, color)){
+      printf("Error drawing line  %d\n", i);
+      return 1;
+    }
   }
   return 0;
 }
--- lab5/lab5.c
+++ lab5/lab5.c
@@ -6,9 +6,9 @@
 #include <stdio.h>
 
 #include "graphics.h"
+#include "keyboard.h"
 
-
-// Any header files included below this line should have been created by you
+extern uint8_t output;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -36,34 +36,83 @@ int main(int argc, char *argv[]) {
 
 int(video_test_init)(uint16_t mode, uint8_t delay) {
   if(set_graphics_mode(mode)){
-<<<<<<< Updated upstream
-    printf("video_test_init() -> Error setting graphics mode\n");
-=======
     printf("Error changing to graphics mode\n");
->>>>>>> Stashed changes
     return 1;
   }
 
   sleep(delay);
 
   if(vg_exit()){
-<<<<<<< Updated upstream
-    printf("vg_exit() -> Error returning to text mode\n");
-=======
     printf("Error returning to text mode\n");
->>>>>>> Stashed changes
     return 1;
   }
   return 0;
 }
 
+int(wait_esq)(){
+  int ipc_status, r;
+  message msg;
+  uint8_t keyboard_bit_no;
+
+  if(keyboard_subscribe_interrupts(&keyboard_bit_no)){
+    printf("Error subscribing keyboard interrupts\n");
+    return 1;
+  }
+  while(output != BREAK_ESC){
+    if((r = driver_receive(ANY, &msg, &ipc_status))) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & BIT(keyboard_bit_no)) {
+            kbc_ih();
+            break;
+          }
+        default:
+          break;
+      }
+    } 
+  }
+
+  if(keyboard_unsubscribe_interrupts()){
+    printf("Error unsubscribing keyboard interrupt's\n");
+    return 1;
+  }
+  return 0;
+}
+
+
 int(video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
                           uint16_t width, uint16_t height, uint32_t color) {
-  /* To be completed */
-  printf("%s(0x%03X, %u, %u, %u, %u, 0x%08x): under construction\n",
-         __func__, mode, x, y, width, height, color);
+  //1. map the video memory to the process' address space
+  if(map_phys_mem(mode)){
+    printf("Error mapping video memory to the process address space\n");
+    return 1;
+  }
 
-  return 1;
+  //2. Change the video mode to that in its argument
+  if(set_graphics_mode(mode)){
+    printf("Error changing to the graphics mode\n");
+    return 1;
+  }
+
+  //3. Draw a rectangle
+  if(vg_draw_rectangle(x, y, width, height, color)){
+    printf("Error drawing rectangle\n");
+    return 1;
+  }
+
+  //4. Reset the video mode to Minix's default text mode and return, upon receiving the break code of the ESC key (0x81)
+  wait_esq();
+
+  if(vg_exit()){
+    printf("Error returning to text mode\n");
+    return 1;
+  }
+  return 0;
 }
 
 int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step) {
@@ -96,3 +145,5 @@ int(video_test_controller)() {
 
   return 1;
 }
+
+
